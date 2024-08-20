2f7521cf01cc5b5113ea5295c6fa0bb80efbc31a
Edward King <king8three@gmailcom>
2024-08-19 23:51:11 -0400

    6 tests passing

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,18 +1,23 @@
 const Table = ({ sat }) => {
   return (
     <table>
-    <thead>
-     <tr>
-       <th>Header TBD</th>
-     </tr>
-     </thead>
-     <tbody>
-     <tr>
-       <td>Row Data TBD</td>
-     </tr>
-     </tbody>
-   </table>
-  );
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Type of Satellite</th>
+          <th>Launch Date</th>
+          <th>Status</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Row Data TBD</td>
+         
+          
+        </tr>
+      </tbody>
+    </table>
+  )
 };
 
 export default Table;
