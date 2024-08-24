8d234eaa48f249b5d0984305ea7ba05da2ad0dec
Edward King <king8three@gmailcom>
2024-08-24 12:57:28 -0400

    Ready to Demo

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -12,13 +12,13 @@ const Table = ({ sat }) => {
         </tr>
       </thead>
       <tbody>
-      {sat.map((id, data) => {
+      {sat.map((data, id) => {
           return (
             <tr key={id}>
-              <td>{id.name}</td>
-              <td>{id.type}</td>
-              <td>{id.launchDate}</td>
-              <td>{id.operational === true ? "Active" : "Inactive"}</td>
+              <td>{data.name}</td>
+              <td>{data.type}</td>
+              <td>{data.launchDate}</td>
+              <td>{data.operational === true ? "Active" : "Inactive"}</td>
             </tr>
           )
         })}
